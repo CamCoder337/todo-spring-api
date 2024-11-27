b65beaf438ee094c93a7a6271db84f4a43c6b8a2
fredtchiadeu <fred.tchiadeu@pategou-consulting.com>
2024-11-27 17:10:42 +0100

    update: loading env vars

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -1,10 +1,14 @@
 package com.example.todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class TodoApplication {
+	@Autowired
+	private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
