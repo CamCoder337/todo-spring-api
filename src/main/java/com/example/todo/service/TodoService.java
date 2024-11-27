b8dcd641ceb4e4619ef8dc05f8a17ef75756dede
fredtchiadeu <fred.tchiadeu@pategou-consulting.com>
2024-11-27 15:00:34 +0100

    cleaning ⚡

--- src/main/java/com/example/todo/service/TodoService.java
+++ src/main/java/com/example/todo/service/TodoService.java
@@ -5,7 +5,7 @@ import com.example.todo.repository.TodoRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
+
 import java.util.List;
 import java.util.Optional;
 
