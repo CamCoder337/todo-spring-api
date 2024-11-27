c2645e703ff678320d1afcd049a3189e4b6e8631
fredtchiadeu <fred.tchiadeu@pategou-consulting.com>
2024-11-27 14:59:30 +0100

    update: handling get and update in controller

--- src/main/java/com/example/todo/controller/ToDoController.java
+++ src/main/java/com/example/todo/controller/ToDoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/todos")
@@ -27,6 +28,18 @@ public class ToDoController {
     public ResponseEntity<List<Todo>> fetchAllTodos(){
         List<Todo> todos = todoService.collectAllTodo();
         return ResponseEntity.ok(todos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Todo> fetchATodo(@PathVariable Long id){
+        Todo todo = todoService.getATodo(id);
+        return ResponseEntity.ok(todo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Todo> updateATodo(@PathVariable Long id, @RequestBody Todo updatedTodo){
+        Todo updateTodoSaved = todoService.UpdateATodo(id, updatedTodo);
+        return ResponseEntity.ok(updateTodoSaved);
     }
 
     @DeleteMapping("/{id}")
