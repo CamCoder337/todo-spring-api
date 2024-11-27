b19a1a59db4269ce12bcf9248c87afc20aca3f3b
fredtchiadeu <fred.tchiadeu@pategou-consulting.com>
2024-11-27 14:57:11 +0100

    update: improving Todo Content

--- src/main/java/com/example/todo/entity/Todo.java
+++ src/main/java/com/example/todo/entity/Todo.java
@@ -19,9 +19,25 @@ public class Todo {
     private String description;
 
     @Column(nullable = false)
-    private int priority;
+    private int priority = 0;
 
-    @Column(nullable = false)
-    private String status;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, columnDefinition = "todo_status DEFAULT 'TODO'")
+    private TodoStatus status;
 
+    public Todo(Long id) {
+        this.id = id;
+    }
+
+    public Todo() {
+
+    }
+
+    public void setStatus(TodoStatus status) {
+        this.status = status;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }
